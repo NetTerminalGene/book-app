d5f007881134989e90f4f465385c2e5574ad8d99
NetTerminalGene <flannoryb@gmail.com>
2025-05-18 17:31:49 -0400

    added reading list page

--- app/list.tsx
+++ app/list.tsx
@@ -1,9 +1,9 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-export default function Index() {
+export default function ListScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <Text style={styles.text}>Reading list</Text>
     </View>
   );
 }
@@ -12,8 +12,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
     color: '#fff',
